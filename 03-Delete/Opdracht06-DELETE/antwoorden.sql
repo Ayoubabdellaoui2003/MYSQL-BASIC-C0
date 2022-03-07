19c3aa18a84778a524c201078d1baaa2dd492c18
Ayoub Abdellaoui <ayoub.abdellaoui@student.rocva.nl>
2022-03-07 16:55:53 +0100

    Opdracht 06 af

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,14 @@
 -- Opdracht 1 
+DELETE FROM players WHERE name="david silva"
 
 -- Opdracht 2 
+DELETE FROM players WHERE club="willem II"
 
--- Opdracht 3
+-- Opdracht 
+DELETE FROM players WHERE club="fc Barcelona" AND nationality="Belgium"
 
 -- Opdracht 4
+DELETE FROM players WHERE age>="38"
 
 -- Opdracht 5
+DELETE FROM players WHERE id="167905" OR id="169595"
